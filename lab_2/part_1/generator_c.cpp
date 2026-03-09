b87900e9995af148a94751162366cd0ee6f754f6
SlipenchukNikita <nikitaslipenchuk.70@mail.ru>
2026-03-09 17:34:23 +0400

    fix gen on c++

--- lab_2/part_1/generator_c.cpp
+++ lab_2/part_1/generator_c.cpp
@@ -6,10 +6,11 @@ using namespace std;
 int main() {
 	ofstream file;
 	file.open("seq_c.txt");
-	random_device eng;
+	random_device rd; 
+	mt19937 gen(rd()); 
 	uniform_int_distribution<int> dist(0, 1);
 	for (int i = 0; i < 128; i++) {
-		file << dist(eng); 
+		file << dist(gen); 
 	}
 	file.close();
 }
